cf250b25b5f9bb3d8a596d0445c556af333ff155
souant <1032911889@qq.com>
2022-12-14 11:45:28 +0800

    feat: 中文提示

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -3,6 +3,11 @@ export default {
   title: 'Note Book',
   description: '一个静态的笔记网站.',
   themeConfig: {
+    outlineTitle: '页面导航',
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
     nav: [
       { text: 'JS', link: '/docs/javascript/' },
       { text: 'JAVA', link: '/docs/java/' },

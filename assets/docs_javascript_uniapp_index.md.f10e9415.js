beecf2391c11d25d162925cc775f63ff30bce77a
souant <1032911889@qq.com>
2022-12-19 18:01:32 +0800

    deploy

--- assets/docs_javascript_uniapp_index.md.f10e9415.js
+++
@@ -1,8 +0,0 @@
-import{_ as a,c as s,o as e,d as n}from"./app.08834a3c.js";const v=JSON.parse('{"title":"uniapp","description":"","frontmatter":{},"headers":[{"level":2,"title":"环境搭建","slug":"环境搭建","link":"#环境搭建","children":[]},{"level":2,"title":"启动开发","slug":"启动开发","link":"#启动开发","children":[]}],"relativePath":"docs/javascript/uniapp/index.md"}'),p={name:"docs/javascript/uniapp/index.md"},i=n(`<h1 id="uniapp" tabindex="-1">uniapp <a class="header-anchor" href="#uniapp" aria-hidden="true">#</a></h1><h2 id="环境搭建" tabindex="-1">环境搭建 <a class="header-anchor" href="#环境搭建" aria-hidden="true">#</a></h2><div class="language-sh"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">npm i -g @vue/cli</span></span>
-<span class="line"></span>
-<span class="line"><span style="color:#A6ACCD;">vue create -p dcloudio/uni-preset-vue my-project</span></span>
-<span class="line"></span>
-<span class="line"></span></code></pre></div><h2 id="启动开发" tabindex="-1">启动开发 <a class="header-anchor" href="#启动开发" aria-hidden="true">#</a></h2><div class="language-sh"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki"><code><span class="line"></span>
-<span class="line"><span style="color:#A6ACCD;">yarn serve</span></span>
-<span class="line"></span>
-<span class="line"></span></code></pre></div>`,5),l=[i];function c(t,o,r,d,h,u){return e(),s("div",null,l)}const C=a(p,[["render",c]]);export{v as __pageData,C as default};

eca4a2fe530793587976d9e57393bd11d1240b9b
souant <1032911889@qq.com>
2023-05-11 10:24:41 +0800

    build

--- docs/html/example/progress/dashboard-progress.vue
+++ docs/html/example/progress/dashboard-progress.vue
@@ -6,12 +6,20 @@
 
 <script setup>
 import { onMounted } from "vue";
-/**
- * [顺时针方向画图，起始点在左侧]
- * @param  {[number]} percent [所占的进度百分比，比如66%，则传66即可，0 <= percent <= 100]
- * @param  {[number]} sR      [圆弧起始角度，可不传，默认是π/2，Math.PI/2 <= sR < 3/2 * Math.PI]
- */
 
+onMounted(() => {
+  // 绘制进度条
+  draw(50, {
+    canvas: document.querySelector("#canvas2"),
+    lineWidth: 10,
+  });
+});
+
+/**
+ * 绘制方法
+ * @param {*} percent 进度
+ * @param {*} options 选项(可选)
+ */
 function draw(percent, options) {
   // 判断参数
   if (percent < 0 || percent > 100) {
@@ -27,83 +35,152 @@ function draw(percent, options) {
   }
   // 默认
   const defaultOptions = {
+    canvas: document.querySelector("#canvas"),
     size: 200,
+    lineWidth: 20,
     sR: 0.8 * Math.PI,
     eR: 2.2 * Math.PI,
     baseColor: "#e1e1e1",
     coverColor: "#fe4d43",
+    textFont: "40px PT Sans",
   };
   // options
-  const { size, sR, eR, baseColor, coverColor } = options || defaultOptions;
+  const rawOptions = Object.assign({}, defaultOptions, options);
+  const { canvas, sR, eR } = rawOptions;
 
   // 限制开始弧度值
   if (sR < Math.PI / 2 || sR >= (3 / 2) * Math.PI) {
     return;
   }
   // canvas
-  let canvas = document.querySelector("#canvas2"),
-    ctx = canvas.getContext("2d"),
-    cWidth = canvas.width,
-    cHeight = canvas.height;
+  let ctx = canvas.getContext("2d");
+  let cWidth = canvas.width;
+  let cHeight = canvas.height;
 
-  let currentPercent = 0; // 当前比例
   let step = (eR - sR) / 100; // 一个1%对应的弧度大小
-  let finalRadian = sR + step * percent; // 终点弧度
 
-  // 首次
-  window.requestAnimationFrame(paint);
+  const paintOptions = {
+    ...rawOptions,
+    ctx,
+    cWidth,
+    cHeight,
+    step,
+  };
+  // 直接绘制
+  // paint(percent, paintOptions, () => {
+  //   console.log(percent);
+  // });
 
-  // 绘制
-  function paint() {
-    ctx.clearRect(0, 0, cWidth, cHeight);
-    let endRadian = sR + currentPercent * step;
-    // 画灰色圆弧
-    drawCanvas(cWidth / 2, cHeight / 2, size / 2, sR, eR, baseColor, 20);
-    // 画红色圆弧
-    drawCanvas(
-      cWidth / 2,
-      cHeight / 2,
-      size / 2,
-      sR,
-      endRadian,
-      coverColor,
-      20
-    );
-
-    // 画红色圆头
-    // 红色圆头其实就是一个圆，关键的是找到其圆心，涉及到三角函数知识，自己画个图一看就明了
-    let angle = 2 * Math.PI - endRadian; // 转换成逆时针方向的弧度（三角函数中的）
-    let xPos = (Math.cos(angle) * size) / 2 + cWidth / 2; // 红色圆 圆心的x坐标
-    let yPos = (-Math.sin(angle) * size) / 2 + cHeight / 2; // 红色圆 圆心的y坐标
-    drawCanvas(xPos, yPos, 2, 0, 2 * Math.PI, coverColor, 25);
-
-    // 数字
-    ctx.fillStyle = coverColor;
-    ctx.font = "40px PT Sans";
-    let textWidth = ctx.measureText(currentPercent + "%").width;
-    ctx.fillText(
-      currentPercent + "%",
-      cWidth / 2 - textWidth / 2,
-      cHeight / 2 + 15
-    );
-    currentPercent++;
-
-    if (endRadian.toFixed(2) < finalRadian.toFixed(2)) {
-      window.requestAnimationFrame(paint);
-    }
+  // 开始进度
+  let currentPercent = 0;
+  // 动画绘制函数
+  function animation(options) {
+    window.requestAnimationFrame(() => {
+      currentPercent++;
+      // 结束绘制判断
+      if (currentPercent > percent) {
+        return;
+      }
+      paint(currentPercent, options, () => {
+        // console.log(currentPercent);
+        animation(options);
+      });
+    });
   }
-  // 绘制形状
-  function drawCanvas(x, y, r, sRadian, eRadian, color, lineWidth) {
-    ctx.beginPath();
-    ctx.lineCap = "round";
-    ctx.strokeStyle = color;
-    ctx.lineWidth = lineWidth;
-    ctx.arc(x, y, r, sRadian, eRadian, false);
-    ctx.stroke();
-  }
+  // 开始动画绘制
+  animation(paintOptions);
 }
 
-onMounted(() => {
-  draw(50);
-});
+// 绘制
+function paint(currentPercent, options, callback) {
+  const {
+    ctx,
+    cWidth,
+    cHeight,
+    size,
+    sR,
+    eR,
+    step,
+    baseColor,
+    coverColor,
+    lineWidth,
+    textFont,
+  } = options;
+
+  // 计算当前绘制结束位置
+  let endRadian = sR + currentPercent * step;
+
+  // 清理画布
+  ctx.clearRect(0, 0, cWidth, cHeight);
+
+  // 画底色圆弧
+  drawArc({
+    ctx,
+    x: cWidth / 2,
+    y: cHeight / 2,
+    r: size / 2,
+    sRadian: sR,
+    eRadian: eR,
+    color: baseColor,
+    lineWidth,
+  });
+  // 画进度圆弧
+  drawArc({
+    ctx,
+    x: cWidth / 2,
+    y: cHeight / 2,
+    r: size / 2,
+    sRadian: sR,
+    eRadian: endRadian,
+    color: coverColor,
+    lineWidth,
+  });
+
+  // 画进度圆头
+  let angle = 2 * Math.PI - endRadian; // 转换成逆时针方向的弧度（三角函数中的）
+  let xPos = (Math.cos(angle) * size) / 2 + cWidth / 2; // 红色圆 圆心的x坐标
+  let yPos = (-Math.sin(angle) * size) / 2 + cHeight / 2; // 红色圆 圆心的y坐标
+  drawArc({
+    ctx,
+    x: xPos,
+    y: yPos,
+    r: 2,
+    sRadian: 0,
+    eRadian: 2 * Math.PI,
+    color: coverColor,
+    lineWidth: lineWidth * 1.25,
+  });
+
+  // 画中心进度数字
+  let text = currentPercent + "%";
+  drawText({
+    ctx,
+    x: cWidth / 2,
+    y: cHeight / 2 + 15,
+    text,
+    fillStyle: coverColor,
+    font: textFont,
+  });
+
+  // 绘制完成，回调
+  callback();
+}
+
+// 绘制圆形状
+function drawArc({ ctx, x, y, r, sRadian, eRadian, color, lineWidth }) {
+  ctx.beginPath();
+  ctx.lineCap = "round";
+  ctx.strokeStyle = color;
+  ctx.lineWidth = lineWidth;
+  ctx.arc(x, y, r, sRadian, eRadian, false);
+  ctx.stroke();
+}
+
+// 绘制文本
+function drawText({ ctx, x, y, text, fillStyle, font }) {
+  ctx.fillStyle = fillStyle;
+  ctx.font = font;
+  let textWidth = ctx.measureText(text).width;
+  ctx.fillText(text, x - textWidth / 2, y);
+}
 </script>
